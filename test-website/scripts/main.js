7bdcdd5c4140b9d4288316632f148672f8d71133
grahamverchere <43447121+grahamverchere@users.noreply.github.com>
2023-09-11 13:00:32 -0700

    testing github pages fr now

---
+++ test-website/scripts/main.js
@@ -0,0 +1,9 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+function multiply(num1, num2) {
+    let result = num1 * num2
+    return result
+}
+document.querySelector("ul").addEventListener("click", function() {
+    alert("you like that aquamarine huh");
+});
